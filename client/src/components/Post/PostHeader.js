179107d5dddc396bf921a2164c434c06e010262c
kishor neupane <kishorliv@gmail.com>
2020-02-16 23:05:56 +1100

    Add link to the post title.

--- client/src/components/Post/PostHeader.js
+++ client/src/components/Post/PostHeader.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const PostHeader = ({ title, author, postDate }) => {
+    const link = '/'+title;
     return(
         <div>
-            <a href="#">{title}</a>
+            <Link to={link}>{title}</Link>
             <div>
                 <i className="fas fa-user">&nbsp;</i>{author}
                 <span>&nbsp;| </span>{postDate}
