0f19acd01a3254e7de88422c629b8c66ce4d1d9b
kishor neupane <kishorliv@gmail.com>
2020-02-16 23:04:19 +1100

    Add routing.

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import { PostList } from './components/Post';
+import { HomePage } from './pages/HomePage';
+import * as ROUTES from './constants/routes';
 
 function App() {
 
-  const posts = [
-    {
-      postId: 541,
-      title: "Why tech is not so useful these days.",
-      author: "Asmita Gaire",
-      postDate: "24-09-2019",
-      content: "slkjf aslkfdjlas lksj fs a vlkjsdf lkasjdfasl dfjlas dfas skfjlaks skdfj asfj lasfj alskdjf laskdfjklas dflkjas df asdlkf ",
-      tags: ['tech', 'nodejs', 'people']
-    },
-    {
-      postId: 120,
-      title: "hjdf djf kdfhjk",
-      author: "Kisser",
-      postDate: "24-09-2019",
-      content: "slkjf dfsdsdf sdfsdfsd sdfsdf sdfsdf sdfs lkasjdfasl dfjlas dfas skfjlaks skdfj asfj lasfj alskdjf laskdfjklas dflkjas df asdlkf ",
-      tags: ['nodejs', 'people']
-    }
-  ];
-
   return (
-    <div className="container border">
-      <p>SourGrapes blog</p>
-      <PostList posts={posts} />
+    <div>
+      <Switch>
+        <Route exact path={ROUTES.HOME} component={HomePage} />
+        <Route path='' render={() => (<p>Sorry, page not found.</p>)} />
+      </Switch>
     </div>
-  );
+    );
 }
 
 export default App;
