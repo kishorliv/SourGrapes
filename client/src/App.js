e27d2dd664c784f2c5118b97ee354a2e343365da
kishor neupane <kishorliv@gmail.com>
2020-02-23 21:43:44 +1100

    Add app routes and use dummy data to test.

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,55 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import { HomePage } from './pages/HomePage';
 import * as ROUTES from './constants/routes';
+import { BlogPage } from './pages/BlogPage';
 
-function App() {
 
-  return (
-    <div>
-      <Switch>
-        <Route exact path={ROUTES.HOME} component={HomePage} />
-        <Route path='' render={() => (<p>Sorry, page not found.</p>)} />
-      </Switch>
-    </div>
-    );
+const posts = [
+  {
+    postId: 541,
+    title: "Why tech is not so useful these days.",
+    author: "Asmita Gaire",
+    postDate: "24-09-2019",
+    content: "slkjf aslkfdjlas lksj fs a vlkjsdf lkasjdfasl dfjlas dfas skfjlaks skdfj asfj lasfj alskdjf laskdfjklas dflkjas df asdlkf ",
+    tags: ['tech', 'nodejs', 'people']
+  },
+  {
+    postId: 120,
+    title: "hjdf",
+    author: "Kisser",
+    postDate: "24-09-2019",
+    content: "slkjf dfsdsdf sdfsdfsd sdfsdf sdfsdf sdfs lkasjdfasl dfjlas dfas skfjlaks skdfj asfj lasfj alskdjf laskdfjklas dflkjas df asdlkf ",
+    tags: ['nodejs', 'people']
+  }
+];
+
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      //posts: null, update from api later
+      posts: posts
+    };
+  }
+
+  componentDidMount(){
+    // call api to fetch posts data
+  }
+
+  render(){
+    return (
+      <div>
+        <Switch>
+          <Route exact path={ROUTES.HOME} render={() => (<HomePage posts={this.state.posts} />)} />
+          <Route path='/:blogTitle' render={() => (<BlogPage />)} />
+          <Route path='' render={() => (<p>Sorry, page not found.</p>)} />
+        </Switch>
+      </div>
+      );
+  }
+
 }
 
 export default App;
